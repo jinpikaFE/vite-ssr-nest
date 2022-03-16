6b81d620207fe3aa29cfa42ed8c1b0fb489ad00b
jinxin_fl <972784646@qq.com>
2022-03-16 14:07:43 +0800

    chore: 使用vite社区ssr模版

--- src/client/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import viteSSR from 'vite-ssr';
-import routes from '@config/routes';
-import App from './App'
-
-export default viteSSR(App, { routes }, ({ url, isClient, request }) => {
-  // Custom initialization hook
-});
