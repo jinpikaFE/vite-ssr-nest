ed60cc7c4fd0c91dbf73eb9d539d8dc11f0449b2
jinxin_fl <972784646@qq.com>
2022-03-18 17:47:05 +0800

    chore: nest在融合

--- pages/test/index.page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-export { Page };
-function Page() {
-    return (React.createElement(React.Fragment, null,
-        React.createElement("h1", null, "test"),
-        React.createElement("p", null, "test")));
-}
